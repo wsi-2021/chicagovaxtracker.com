6a498e3823b336f6601a0e5113ad565d5572a355
Karl Stolley <karl.stolley@gmail.com>
2021-03-31 16:35:11 -0500

    Use Index model in the index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var {Index} = require('../models/index');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:id', function(req, res, next) {
+  let my_index = new Index('Hello from Model-World','Here is nice, fresh content');
+  res.render('index', my_index);
 });
 
 module.exports = router;
