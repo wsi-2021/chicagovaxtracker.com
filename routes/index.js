6c52da1c6074985f0c1d8fdb3cb635dc6d78b286
Karl Stolley <karl.stolley@gmail.com>
2021-04-18 12:42:02 -0500

    Remove :id variable from index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var {Index} = require('../models/index');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
+router.get('/', function(req, res, next) {
   let my_index = new Index('Hello from Model-World','Here is nice, fresh content');
   res.render('index', my_index);
 });
