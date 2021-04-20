ef611de7c05ce2d665385cfbdda5769fc3c5ccf6
Karl Stolley <karl.stolley@gmail.com>
2021-04-20 14:13:05 -0500

    Remove unused 'next' vars

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var {Index} = require('../models/index');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   let my_index = new Index('Hello from Model-World','Here is nice, fresh content');
   res.render('index', my_index);
 });
