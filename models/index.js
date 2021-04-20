567fe31ced7ee9f36ccdd17d1604fdcf7a9eb640
Karl Stolley <karl.stolley@gmail.com>
2021-04-20 14:16:09 -0500

    Appease the linter

--- models/index.js
+++ models/index.js
@@ -12,4 +12,4 @@ class Index {
 
 module.exports = {
   Index
-}
+};
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  let my_index = new Index('Hello from Model-World','Here is nice, fresh content');
+  let my_index = new Index('Hello from Model-World', 'Here is nice, fresh content');
   res.render('index', my_index);
 });
 
