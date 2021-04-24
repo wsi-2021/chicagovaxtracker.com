64094e4627732156e52a792309f6d5d52be11ea9
Karl Stolley <karl.stolley@gmail.com>
2021-04-24 13:11:11 -0500

    Progressively enhance the days navigation

--- public/js/main.js
+++ public/js/main.js
@@ -26,3 +26,41 @@ function sortTable(table, columnIndex, order) {
 
   table.querySelector('tbody').append(...rows);
 }
+
+var days_nav = document.querySelector('ul.days');
+
+days_nav.addEventListener('click', function(e) {
+  if (e.target.tagName !== 'A') return; // leave
+  e.preventDefault();
+  var href = e.target.getAttribute('href');
+  var api_endpoint = '/api' + href;
+  fetch(api_endpoint)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var tbody = document.querySelector('table#records tbody');
+      tbody.innerHTML = "";
+      for (d of data) {
+        var tr = document.querySelector('template#tr').content.cloneNode(true);
+        var td = tr.querySelectorAll('td');
+        var dt = new Date(d.date);
+        var pretty_date = `${dt.getMonth() + 1}/${dt.getDate()}/${dt.getFullYear()}`;
+        td[0].dataset.value = d.date;
+        td[0].innerText = pretty_date;
+        td[1].dataset.value = d.total_doses_daily;
+        td[1].innerText = d.total_doses_daily;
+        td[2].dataset.value = d.total_doses_cumulative;
+        td[2].innerText = d.total_doses_cumulative;
+        tbody.append(tr);
+      }
+
+      if (history.pushState) {
+        history.pushState('', '', href);
+      }
+
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
