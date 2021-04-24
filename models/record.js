f04fb5baaf3a387cba6c45a6e94b6f5694858e3c
Karl Stolley <karl.stolley@gmail.com>
2021-04-23 19:19:58 -0500

    Clean up old comments

--- models/record.js
+++ models/record.js
@@ -24,12 +24,6 @@ class Record {
       const connection = await mdb.connect();
       const db = connection.db(`${process.env.NODE_ENV}--vax-data`);
       const collection = db.collection('by_zip');
-      // The variables on the query are:
-      //   - the zip code (if it exists, aka isn't "all")
-      //   - the limit (60 on 'all', requested as such on per-zip; default 10, 30, 0 [all])
-      // Per zip-code
-      // const cursor = collection.find({zip_code: "60641"}, {sort: [['date',-1],['zip_code',1]], limit: 10});
-      // Latest, all zip codes; the limit ensures the 59 Chicago Zips + Unknown
       const cursor = collection.find(this.query, {sort: [['date',-1],['zip_code',1]], limit: this.limit});
       items = await cursor.toArray();
     } catch(e) {
